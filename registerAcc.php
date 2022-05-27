9a9816d0f52ae0fd5af9d224e984397d0a260ce7
Staniell <giostaniell12@gmail.com>
2022-05-27 21:15:04 +0800

    changed username to email for validation

--- registerAcc.php
+++ registerAcc.php
@@ -6,7 +6,7 @@ $email =  $_GET['email'];
 $passwordx = $_GET['password'];
 
 //Checking if username is already taken
-if(mysqli_num_rows(mysqli_query($con,"SELECT username FROM `user_accounts` WHERE username LIKE '".$username."'"))!=0)
+if(mysqli_num_rows(mysqli_query($con,"SELECT email FROM `user_accounts` WHERE email LIKE '".$email."'"))!=0)
     echo "The name ". $username ." is already taken. Please insert another name.";
 else{
     $result = mysqli_query($con,"INSERT INTO user_accounts (username,email,password) VALUES('$username','$email','$passwordx')");
