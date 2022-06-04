aa1b39d180a4a402f59b27906d7eb98dfe980ce3
Staniell <giostaniell12@gmail.com>
2022-06-05 00:23:52 +0800

    fetch orders updated

--- fetch_orders.php
+++ fetch_orders.php
@@ -2,9 +2,10 @@
 
 include_once('connects.php');
 $email = $_GET['email'];
+$order_type = $_GET['order_type'];
 
-$query = "SELECT book_title, order_id, book_price, order_table.book_id, email, order_status, order_date FROM `order_table` INNER JOIN book_table on book_table.book_id = order_table.book_id WHERE email='".$email."' and order_status REGEXP 'pending|shipped'";
-// $query = "SELECT book_title, order_id, book_price, order_table.book_id, email, order_status, order_date FROM `order_table` INNER JOIN book_table on book_table.book_id = order_table.book_id WHERE email='".$email."' and order_status != 'Received' or order_status != 'Cancelled'";
+$query = "SELECT book_title, order_id, book_price, order_table.book_id, email, order_status, order_date FROM `order_table` INNER JOIN book_table on book_table.book_id = order_table.book_id WHERE email='".$email."' and order_status REGEXP '".$order_type."'";
+
 $check=mysqli_query($con,$query);
 $row=mysqli_num_rows($check);
 $myArray = array();
