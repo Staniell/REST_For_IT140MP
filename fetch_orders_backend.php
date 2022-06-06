fd823d540507117d5d3b103e1dd0a855a8553d1d
Staniell <giostaniell12@gmail.com>
2022-06-06 21:57:01 +0800

    added address for admin orders and registration

--- fetch_orders_backend.php
+++ fetch_orders_backend.php
@@ -2,7 +2,8 @@
 
 include_once('connects.php');
 
-$query = "SELECT book_title, order_id, book_price, order_table.book_id, email, order_status, order_date FROM `order_table` INNER JOIN book_table on book_table.book_id = order_table.book_id";
+// $query = "SELECT book_title, order_id, book_price, order_table.book_id, email, order_status, order_date FROM `order_table` INNER JOIN book_table on book_table.book_id = order_table.book_id";
+$query = "SELECT book_title, order_id, book_price, home_address, order_table.book_id, order_table.email, order_status, order_date FROM `order_table` INNER JOIN book_table on book_table.book_id = order_table.book_id INNER JOIN user_accounts on order_table.email = user_accounts.email";
 
 $check=mysqli_query($con,$query);
 $row=mysqli_num_rows($check);
--- registerAcc.php
+++ registerAcc.php
@@ -4,12 +4,13 @@ include_once('connects.php');
 $username = $_GET['username'];
 $email =  $_GET['email'];
 $passwordx = $_GET['password'];
+$home_address = $_GET['home_address'];
 
 //Checking if username is already taken
 if(mysqli_num_rows(mysqli_query($con,"SELECT email FROM `user_accounts` WHERE email LIKE '".$email."'"))!=0)
     echo "The name ". $username ." is already taken. Please insert another name.";
 else{
-    $result = mysqli_query($con,"INSERT INTO user_accounts (username,email,password) VALUES('$username','$email','$passwordx')");
+    $result = mysqli_query($con,"INSERT INTO user_accounts (username,email,home_address,password) VALUES('$username','$email','$home_address','$passwordx')");
     echo "Account Registered";
 }
 ?>
